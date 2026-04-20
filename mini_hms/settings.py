1ae34c9175885e1c1a233db01aad78b7fb2a98b0
iotiangyanu <g.dwivedi8924@gmail.com>
2026-04-20 21:47:36 +0530

    requierments file is sdded

--- mini_hms/settings.py
+++ mini_hms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$kkj1h(dgig74do=n3$4dcvb#2^4k#$6nb))12v(x_kgbnf4vi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
