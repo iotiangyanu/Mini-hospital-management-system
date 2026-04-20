fc21c2af069bbd973a9dd1f788ddd08e63c93171
iotiangyanu <g.dwivedi8924@gmail.com>
2026-04-20 23:01:49 +0530

    requirement change

--- doctors/views.py
+++ doctors/views.py
@@ -1,4 +1,4 @@
-
+from annotated_types import SLOTS
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
