09369090708ff856523282f29d2b316e57ef395b
iotiangyanu <g.dwivedi8924@gmail.com>
2026-05-11 07:43:46 +0530

    Added production email configuration

--- mini_hms/settings.py
+++ mini_hms/settings.py
@@ -141,3 +141,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("SENDER_EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("SENDER_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
