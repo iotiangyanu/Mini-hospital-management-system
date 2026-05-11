a5f0747e0288c8cf50323add1c4e6996b151e0e0
iotiangyanu <g.dwivedi8924@gmail.com>
2026-05-11 07:13:18 +0530

    update

--- doctors/views.py
+++ doctors/views.py
@@ -1,4 +1,3 @@
-from annotated_types import SLOTS
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
